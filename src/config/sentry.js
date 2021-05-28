aeb999fa58dbdb33e25b5c4e9652c82f9d2e7850
Victor <victortrindade@gmail.com>
2021-05-28 13:11:51 -0300

    .env + sentry + exception handler

---
+++ src/config/sentry.js
@@ -0,0 +1,8 @@
+export default {
+  dsn: 'https://44bad92171fd4fdb8116d26e84966266@o329715.ingest.sentry.io/5790524',
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+};
